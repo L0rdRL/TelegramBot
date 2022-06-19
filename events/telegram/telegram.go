0fd30870d3768817ca60f05942cedfe37d451b8b
Your Name <you@example.com>
2022-06-19 22:09:16 +0600

    add telegram client

---
+++ events/telegram/telegram.go
@@ -0,0 +1,10 @@
+package telegram
+
+type Processor struct {
+	tg     *telegram.Clent
+	offset int
+	// storage
+}
+
+func New(client *telegram.Cliient) {
+}
--- storage/files/files.go
+++ storage/files/files.go
@@ -1,6 +1,7 @@
 package files
 
 import (
+	"encoding/gob"
 	"os"
 	"path/filepath"
 )
@@ -23,4 +24,25 @@ func (s Storage) Save(page *storage.Page) (err error) {
 	if err := os.MkdirAll(filePath, defaultPerm); err != nil {
 		return err
 	}
+	fName, err := fileName(page)
+	if err != nil {
+		return nil
+	}
+
+	filePath := filepath.Join(fPath, fName)
+
+	file,err := os.Create(fPath)
+	if err != nil {
+		return err
+	}
+	defer func() { _=file.Close()}()
+
+	if err:=gob.NewEncoder(file).Encode(page){
+		return err
+	} 
+		return nil 
+}
+
+func fileName(p *storage.Page) (string, error) {
+	return p.Hash()
 }
